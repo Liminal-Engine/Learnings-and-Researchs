b4ec66a0ae7fa8ef00567da7318b898baae65bd6
DE VITA Matteo <matteo.devita7@gmail.com>
2023-06-05 18:10:13 +0200

    remove useless comment

--- Basic App/src/App.cpp
+++ Basic App/src/App.cpp
@@ -12,14 +12,6 @@ App::App(void)
 :
 _window{window::WindowWrapper(800, 600, "Basic App")},
 _vulkan{vulkan::VulkanWrapper(this->_window)}
-//_vulkan{[this]() -> vulkan::VulkanWrapper {
-//     const std::vector<const char *> GLFWExtensions = this->_window.getRequiredVulkanInstanceExtensions();
-//     return vulkan::VulkanWrapper(
-//         GLFWExtensions,
-//         GLFWExtensions.size(),
-//         this->_window.getGLFWWindow()
-//     );
-// }()}
 {
     
 }
